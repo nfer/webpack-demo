46d84341462f3c9da145f59e4e4adc15d02f38e5
Nfer Zhuang <nfer.zhuang@gmail.com>
2017-12-17 17:37:29 +0800

    add html-webpack-plugin

---
+++ demo-02/webpack.config.js
@@ -0,0 +1,10 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: __dirname + '/dist/',
+        filename: 'index.js'
+    },
+    plugins: [new HtmlWebpackPlugin()]
+}
