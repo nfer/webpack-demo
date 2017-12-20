45257d79cc722a280693b66235671889ee49dfdc
Nfer Zhuang <nfer.zhuang@gmail.com>
2017-12-20 20:59:27 +0800

    add demo-05(copy from demo-04)

---
+++ demo-05/webpack.config.js
@@ -0,0 +1,27 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: __dirname + '/dist/',
+        filename: 'index.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    use: ['css-loader']
+                })
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'webpack-demo-05'
+        }),
+
+        new ExtractTextPlugin('index.css'),
+    ]
+}
