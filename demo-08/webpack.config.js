ecc7374aed8325f1812ef52c6c30ac87187b9aca
Nfer Zhuang <nfer.zhuang@gmail.com>
2017-12-23 13:26:40 +0800

    add demo-08(copy from demo-07)

---
+++ demo-08/webpack.config.js
@@ -0,0 +1,29 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: {
+        index: './index.js',
+    },
+    output: {
+        path: __dirname + '/dist/',
+        filename: '[name].[hash:7].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: ExtractTextPlugin.extract({
+                    use: ['css-loader', 'less-loader']
+                })
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'webpack-demo-08'
+        }),
+
+        new ExtractTextPlugin('[name].[hash:7].css'),
+    ]
+}
